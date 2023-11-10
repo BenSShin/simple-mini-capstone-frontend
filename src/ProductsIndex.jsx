74a00b63c030c894acf497a351c60d847e6e2da2
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-10 15:45:03 -0600

    added productsindex connecting api to frontend to get info from database

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,7 +1,16 @@
-export function ProductsIndex() {
+/* eslint-disable react/prop-types */
+export function ProductsIndex(props) {
   return (
     <div>
-      <h1>hi</h1>
+      <h1>All Products</h1>
+      {props.products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <img src={product.image_url} alt="img" />
+          <p>{product.price}</p>
+          <p>{product.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
